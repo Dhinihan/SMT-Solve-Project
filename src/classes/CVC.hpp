6cbc12c9e9d0520986bc5c4fbf6201241e5e0a9f
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2014-03-14 17:49:02 -0300

    Created the method to create the output for prog3

--- src/classes/CVC.hpp
+++ src/classes/CVC.hpp
@@ -16,10 +16,17 @@ class CVC
     public:
         /* Method createFile: create an input for cvc4.
          *
-         * parameter M       : the Matrix with the coeffitients.
+         * parameter M       : the Matrix with the coefficients.
          * parameter fileName: the name of the file to be created
          */
-        static void createFile(Matrix* M, const char* fileName);   
+        static void createFile(Matrix* M, const char* fileName);
+        
+        /* Method createFile: create an input for cvc4.
+         *
+         * parameter input   : A text file with the types of words
+         * parameter fileName: the name of the file to be created
+         */
+        static void createFile(const char* input, const char* fileName);   
 };
 
 #endif
