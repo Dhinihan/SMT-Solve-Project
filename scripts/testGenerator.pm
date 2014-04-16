8f0d5dae70de86b77660be29dde6d7c1844b1898
Vinícius <vinicius.nascimento.silva@usp.br>
2014-04-16 15:29:58 -0300

    Now parse with the constant and n*w

--- scripts/testGenerator.pm
+++ scripts/testGenerator.pm
@@ -4,18 +4,20 @@ use warnings;
 use strict;
 use v5.10;
 
-die "I need parameters!" if @ARGV < 1;
+die "I need parameters!" if @ARGV < 2;
 
 my $n = $ARGV[0];
+my $w = $ARGV[1];
 
 say "N = $n";
+say "W = $w";
 
 open(OUTPUT, ">", "tests/input.txt")
 or die "Impossible to create the file";
 
-print OUTPUT "$n ";
+print OUTPUT "$n $w ";
 
-for(my $i = 0; $i < $n; $i++)
+for(my $i = 0; $i < $n*$w + 1; $i++)
 {
     my $rand    = int(rand(200) - 150);
     print OUTPUT "$rand ";
