d9e4b63330b007525cd9a79a5e82b6b1256e19d8
Vinícius Silva <vinicius.nascimento.silva@usp.br>
2014-07-20 01:41:55 -0300

    Created test generator

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <time.h>
+
 #include "../include/PSATsolver.hpp"
+#include "../include/TestGenerator.hpp"
 
 using namespace std;
 using namespace arma;
@@ -7,6 +10,25 @@ int main(int argc, char** argv)
 {
     bool v = false;
     
+    if (argc < 2)
+    {
+        std::cout << "I need a input file " << "\n";
+        return -1;
+    }
+    
+    if((string) argv[1] == "--test")
+    {
+        TestGenerator t(atoi(argv[2]));
+        t.createAll(atoi(argv[3]),
+                    atoi(argv[4]),
+                    atoi(argv[5]),
+                    atof(argv[6]),
+                    atoi(argv[7]),
+                    atoi(argv[8]),
+                    argv[9]);
+        return 1;
+    }
+
     if (argc < 2)
     {
         std::cout << "I need a input file " << "\n";
