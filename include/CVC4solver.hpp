10e20eec6b3cc3f4d65872944914519fa58d7b1f
Vinícius Silva <vinicius.nascimento.silva@usp.br>
2014-07-26 11:45:03 -0300

    working on test

--- include/CVC4solver.hpp
+++ include/CVC4solver.hpp
@@ -25,7 +25,7 @@ class CVC4Solver
          */
         CVC4Solver();
         
-        static const int delta = 100000;
+        static const int delta = 1000;
         
         //! Creates the variables.
         /*!
