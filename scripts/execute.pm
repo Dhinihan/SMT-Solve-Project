f864a232113a88e177f821a9c11bb7c9f2768652
Vinícius Silva <vinicius.nascimento.silva@usp.br>
2014-07-27 18:47:25 -0300

    Data processed for k12n30

--- scripts/execute.pm
+++ scripts/execute.pm
@@ -4,71 +4,58 @@ use warnings;
 use strict;
 use v5.10;
 
-my $w;
-my $line;
 my $file;
-my $i = 0;
+my $i;
+my $j;
+my @data;
 
 die "I need a file!" if @ARGV == 0;
 
 $file = shift @ARGV;
 
-open(MODEL, "<", "tests/model.txt") 
-or die "Impossible to open the model";
-
-open(OUTPUT, ">", "output.cvc")
-or die "Impossible to create the temporary file";
-
-#Running the program that typify the text
-
-$w = `cat $file | ./dist/prog`;
-
-say "The struct: \n" . $w;
-
-#Print the header in the cvc4 file
-while($line = <MODEL>)
+for($i = 1; $i <= 5; $i++)
 {
-    print OUTPUT $line; 
-}
-
-print OUTPUT "\n % The input % \n";
-
-#Print the input in the model.
-while($w =~ s/(\w)//)
-{
-    given ($1)
+    open(INPUT, "<", $file . $i . '.txt')
+    or die "Impossible to open file\n";
+    
+    while(my $line = <INPUT>)
     {
-        when("V") { print OUTPUT "ASSERT v($i);\n"; }
-        when("N") { print OUTPUT "ASSERT n($i);\n"; }
-        when("D") { print OUTPUT "ASSERT d($i);\n"; }
+        my $unid;
+        my $dec;
+        my $value;
+        
+        $line =~ m/(\d+)[.]?(\d)?\s(\d+[.]?\d*)/;
+        
+        $unid = $1;
+        if (defined($2))
+        {
+            $dec = $2;
+        }  
+        else 
+        {
+            $dec = 0;
+        } 
+        $value = $3;
+        $data[$unid][$dec] += $value/5;
     }
-    $i++;
+    close INPUT;
 }
 
-$i--;
+open(OUTPUT, ">", $file . "_final" . '.txt')
+    or die "Impossible to create output file\n";
+    
 
-print OUTPUT "\n % The output % \n";
+for($i = 1; $i < 10; $i++)
+{
+    for($j = 0; $j < 10; $j++)
+    {
+        print OUTPUT  ($i . '.' . $j . " " . $data[$i][$j] . "\n");
+        print ($i . '.' . $j . " " . $data[$i][$j] . "\n");
+    }
+}
 
-#Print the formula for the answers in the model 
-say OUTPUT "ASSERT x = sum_col_vn($i,$i);";
-say OUTPUT "ASSERT y = sum_col_nd($i,$i);";
-say OUTPUT "ASSERT z = sum_col_dv($i,$i);";
-print OUTPUT "CHECKSAT;\nCOUNTERMODEL;\n";
+print OUTPUT  ('10.0'. " " . $data[10][0] . "\n");
+print ('10.0'. " " . $data[10][0] . "\n");
 
-#Find the answers in the CVC4's output
 
-say "Processing...";
-my $output = `cvc4 -im -L LANG_CVC4 output.cvc`;
-my @dist;
-$output =~ m/x : REAL = (.*);/;
-$dist[0] = $1;
-$output =~ m/y : REAL = (.*);/;
-$dist[1] = $1;
-$output =~ m/z : REAL = (.*);/;
-$dist[2] = $1;
-
-#Print the answers.
-say "The answers:";
-say "distance VN = " . $dist[0];
-say "distance ND = " . $dist[1];
-say "distance DV = " . $dist[2];
+close INPUT;
